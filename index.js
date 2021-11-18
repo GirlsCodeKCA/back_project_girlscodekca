ed12bf0919cb405e62a25c10dfbc6a9f4f1c13ab
carolina.mantilla14@gmail.com <carolina.mantilla14@gmail.com>
2021-11-18 15:50:40 -0500

    Conexión BD

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import conectarBD from './db/db.js';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen({ port: process.env.PORT || 4000 }, async () => {
+  await conectarBD();
+  
+});
